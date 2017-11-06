98402f5532f5cfe2a8118b0416e54fbec30b9784
Andy Cook <cookab1@appstate.edu>
2017-11-06 09:21:51 -0500

    Added first Broadcast Reciever for BOOT_COMPLETED

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by user on 11/6/2017.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+    }
+}
